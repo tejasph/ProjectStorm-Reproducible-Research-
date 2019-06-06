fcd9d36f8518d928f02ac23acab6b114b63a0bcd
tejasph <tphaterp@gmail.com>
2019-06-06 08:56:32 -0700

    Set up R script and Markdown document for the project.

---
+++ ProjectStormWorkspace.R
@@ -0,0 +1,7 @@
+#Project Storm WorkSpace
+
+#Two Main Questions:
+#1. Across United States, which type of events(EVTYPE) are most harmful with respect to 
+#population health?
+
+#2. Across US, which types of events have the greatest economic consequences?
