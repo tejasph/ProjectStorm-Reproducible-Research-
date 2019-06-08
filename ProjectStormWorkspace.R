ba7e24c4018a424433ae5971f50c39727598e517
tejasph <tphaterp@gmail.com>
2019-06-07 17:57:07 -0700

    Started cleaning the data. So far, I filtered out irrelevant columns and time periods.
    Currently working on EVTYPE cleaning.

--- ProjectStormWorkspace.R
+++ ProjectStormWorkspace.R
@@ -5,3 +5,18 @@
 #population health?
 
 #2. Across US, which types of events have the greatest economic consequences?
+
+setwd("~/R Material/Coursera/Course 5 Reproducible Research/Week 4/Project Storm/repdata_data_StormData.csv")
+WeatherDatabase <- read.csv("repdata_data_StormData.csv", stringsAsFactors = FALSE)
+
+library(dplyr)
+#list of variables important to the analysis
+UsefulColumns <- c("EVTYPE", "STATE","BGN_DATE", "BGN_TIME", "TIME_ZONE",
+                   "END_DATE", "END_TIME","FATALITIES", "INJURIES", "PROPDMG",
+                   "PROPDMGEXP", "CROPDMG", "CROPDMGEXP")
+#Retain only the useful variables
+WeatherDatabase <- select(WeatherDatabase, UsefulColumns)
+
+#First, change BGN_DATE from a factor class to a Date Class
+WeatherDatabase$BGN_DATE <- as.Date(WeatherDatabase$BGN_DATE, format = "%m/%d/%Y")
+WeatherDatabase <- filter(WeatherDatabase, BGN_DATE >= "1996-01-01")
